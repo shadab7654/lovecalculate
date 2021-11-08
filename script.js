d45a251ded380f507a847cb0603fb350945a985d
shadab7654 <86181725+shadab7654@users.noreply.github.com>
2021-11-09 02:29:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+prompt("What is your Name ?");
+prompt("What is there Name ?");
+var lovescore = Math.random()*100;
+lovescore = Math.floor(lovescore) + 1;
+if(lovescore >= 80){
+    alert("Your Love Score is " + lovescore + " %" + " You Love each Other");
+
+}
+if(lovescore =30 && lovescore<=80){
+    alert("your love score "+ lovescore + "Start loving Each other");
+}
+if(lovescore<30 && lovescore >= 20){
+    alert("your love score is " + lovescore +  "Fack love");
+}
+// else{
+//     alert("your love score is " + lovescore + "%");
+
+// }
